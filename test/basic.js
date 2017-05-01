a895ec77f0e3e8fb9d85735e86c8b5b50c05d1f5
Kevin Lamping <klamping@gmail.com>
2017-05-01 14:52:40 -0500

    end of demo 2

--- test/basic.js
+++ test/basic.js
@@ -1,9 +1,18 @@
-describe('the test', function () {
-  it('should run', function () {
+var expect = require('chai').expect;
+
+describe('Login Page', function () {
+  it('should be titled properly', function () {
     browser.url('');
 
     var title = browser.getTitle();
 
-    console.log(title);
+    expect(title).to.equal('Login Page')
+  })
+
+  it('should contain the correct url', function () {
+    browser.url('');
+
+    var url = browser.getUrl();
+    expect(url).to.contain('/example/');
   })
 })
